920a0ecdf659c3df3152566019e1aee703610bfa
Fabio Cassimiro <fabiocassimiro.project@gmail.com>
2023-09-13 20:36:12 -0300

    initial commit project

---
+++ src/main/java/com/cassimirotech/customerapiservice/CustomerApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cassimirotech.customerapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApiServiceApplication.class, args);
+    }
+
+}
